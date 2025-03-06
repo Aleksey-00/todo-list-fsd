7dd73f3ac1e4540c646efe2d49b390b7ecce1df0
Alexey Azorabekov <a.azorabekov@presentsimple.com>
2025-03-06 18:01:56 +0300

    Fix errors with deploy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src/")
     }
   },
-  base: '/todo-list-fsd/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
